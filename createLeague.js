0418a670fd345dd6512b6464f9923c8d5526e35d
Chris Burke <burke1791@gmail.com>
2020-01-16 18:34:17 -0600

    created the createLeague lambda

---
+++ createLeague.js
@@ -0,0 +1,37 @@
+import { success, failure } from './libraries/response-lib';
+
+const sql = require('mssql');
+
+const connection = require('./db').connection;
+const verifyToken = require('./libraries/verify').verifyToken;
+
+export async function createLeague(event, context, callback) {
+  try {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    let body = JSON.parse(event.body);
+    let name = body.name;
+    let password = body.password;
+    let year = body.year;
+    let auctionId = 'placeholder'; // @TODO: replace this with the uid from dynamo
+    let cognitoSub = await verifyToken(event.headers['x-cognito-token']);
+
+    if (!connection.isConnected) {
+      await connection.createConnection();
+    }
+
+    const request = new sql.Request();
+    request.input('cognitoSub', sql.VarChar(256), cognitoSub);
+    request.input('name', sql.VarChar(50), name);
+    request.input('password', sql.VarChar(50), password);
+    request.input('year', sql.SmallInt(), year);
+    request.input('auctionId', sql.VarChar(256), auctionId);
+
+    let result = await request.execute('dbo.up_createLeague');
+    console.log(result);
+    callback(null, success({ message: 'success!' }));
+  } catch (error) {
+    console.log(error);
+    callback(null, failure({ message: 'ERROR!' }));
+  }
+}
