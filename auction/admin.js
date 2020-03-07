2c67e356631bb0abf5a8d9bbb8da40a085ae744c
Chris Burke <burke1791@gmail.com>
2020-03-07 08:51:55 -0600

    added functionality to set an auction item to sold

--- auction/admin.js
+++ auction/admin.js
@@ -69,3 +69,80 @@ export async function resetClock(event, context, callback) {
     });
   }
 }
+
+export async function setItemComplete(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  console.log(event);
+  let data = JSON.parse(event.body);
+  let leagueId = data.leagueId;
+  let connectionId = event.requestContext.connectionId;
+
+  if (!connection.isConnected) {
+    await connection.createConnection();
+  }
+
+  const request = new sql.Request();
+  request.input('connectionId', sql.VarChar(128), connectionId);
+  request.input('leagueId', sql.BigInt(), leagueId);
+
+  try {
+    let result = await request.execute('dbo.up_setItemComplete');
+    console.log(result);
+
+    let connectionIds = result.recordset;
+    let auctionStatus = result.recordsets[1][0];
+    console.log(auctionStatus);
+
+    let payload = {
+      msgObj: auctionStatus,
+      msgType: 'auction'
+    };
+
+    await sendWebsocketPayloads(connectionIds, payload, event.requestContext.domainName, event.requestContext.stage, callback);
+
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'messages not sent' })
+    });
+  } catch (error) {
+    console.log(error);
+    callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'messages not sent' })
+    });
+  }
+}
+
+async function sendWebsocketPayloads(connectionIds, payload, domainName, stage) {
+  console.log(domainName, stage);
+
+  const apig = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint: 'https://' + domainName + '/' + stage
+  });
+
+  const postCalls = connectionIds.map(async obj => {
+    console.log(obj);
+    var params = {
+      ConnectionId: obj.connectionId,
+      Data: JSON.stringify(payload)
+    };
+
+    try {
+      await apig.postToConnection(params).promise();
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  });
+
+  try {
+    await Promise.all(postCalls);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  return;
+}
