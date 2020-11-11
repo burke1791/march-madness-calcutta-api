cf0dc7a29cc5d5daede9388a23ef55f0eccbe72e
Chris Burke <burke1791@gmail.com>
2020-11-11 12:33:16 -0600

    added an endpoint for retrieving league settings

--- services/league/functions/leagueSettings.js
+++ services/league/functions/leagueSettings.js
@@ -72,3 +72,29 @@ export async function updateLeagueSettings(event, context, callback) {
     callback(null, { message: 'ERROR!' });
   }
 }
+
+export async function getLeagueSettings(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  try {
+    let cognitoSub = event.cognitoPoolClaims.sub;
+
+    let leagueId = event.path.leagueId;
+
+    if (!connection.isConnected) {
+      await connection.createConnection();
+    }
+
+    let result = await connection.pool.request()
+      .input('LeagueId', BigInt, leagueId)
+      .input('CognitoSub', Varchar(256), cognitoSub)
+      .execute('dbo.up_GetLeagueSettings_Legacy');
+
+    console.log(result);
+
+    callback(null, result.recordset);
+  } catch (error) {
+    console.log(error);
+    callback(null, { message: 'ERROR!' });
+  }
+}
