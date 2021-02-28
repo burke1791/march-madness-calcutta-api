fa0358192446981854ce27f9323fbbedd9aa1333
Chris Burke <burke1791@gmail.com>
2021-02-28 12:45:46 -0600

    created endpoint for fetching seed groups by league

---
+++ services/league/functions/getLeagueSeedGroups.js
@@ -0,0 +1,27 @@
+import { BigInt, Varchar } from '../../../common/utilities/db';
+
+const connection = require('../../../common/utilities/db').connection;
+
+export async function getLeagueSeedGroups(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  let cognitoSub = event.cognitoPoolClaims.sub;
+
+  try {
+    let leagueId = event.path.leagueId;
+
+    if (!connection.isConnected) {
+      await connection.createConnection();
+    }
+
+    let result = await connection.pool.request()
+      .input('LeagueId', BigInt, leagueId)
+      .input('CognitoSub', Varchar(256), cognitoSub)
+      .execute('dbo.up_GetLeagueSeedGroups');
+
+    callback(null, result.recordset);
+  } catch (error) {
+    console.log(error);
+    callback(null, { message: 'ERROR!' });
+  }
+}
