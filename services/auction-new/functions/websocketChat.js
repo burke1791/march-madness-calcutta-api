4d359437a7d98d5a199ec1ee3547b8688f1be20b
Chris Burke <burke1791@gmail.com>
2021-12-31 13:19:30 -0600

    MMC-1: finished first iteration of the test chat lambda

--- services/auction-new/functions/websocketChat.js
+++ services/auction-new/functions/websocketChat.js
@@ -1,20 +1,9 @@
 import AWS from 'aws-sdk';
-import { verifyToken } from '../../../common/utilities/verify';
 
 const dynamodb = new AWS.DynamoDB();
-const lambda = new AWS.Lambda();
 
 const CONNECTION_TABLE = process.env.CONNECTION_TABLE;
 const CHAT_TABLE = process.env.CHAT_TABLE;
-
-const LAMBDAS = {
-  VERIFY_USER_LEAGUE: `calcutta-auction-service-v2-${process.env.APP_ENV}-verifyUserLeague`
-};
-
-const headers = {
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*'
-};
 
 export async function sendChatMessage(event, context, callback) {
   context.callbackWaitsForEmptyEventLoop = false;
@@ -42,6 +31,7 @@ export async function sendChatMessage(event, context, callback) {
     let response = await dynamodb.getItem(params).promise();
     console.log(response);
 
+    // @TODO: verify the leagueId in dynamodb matches the leagueId in the request body
     if (!response?.Item.ConnectionId.S) {
       throw new Error('User does not exist in the Connection table');
     }
@@ -77,15 +67,61 @@ export async function sendChatMessage(event, context, callback) {
       Timestamp: timestamp
     };
 
+    const payload = {
+      msgObj: chatObj,
+      msgType: 'chat'
+    };
+
     let chatResponse = await dynamodb.putItem(chatParams).promise();
     console.log(chatResponse);
+
+    // @TODO: verify the message was sent to the table
+
+    const queryParams = {
+      TableName: CONNECTION_TABLE,
+      ExpressionAttributeValues: {
+        ':v1': {
+          N: leagueId
+        }
+      },
+      KeyConditionExpression: 'LeagueId = :v1',
+      ProjectionExpression: 'ConnectionId'
+    };
+
+    const connectionIdQuery = await dynamodb.query(queryParams).promise();
+    console.log(connectionIdQuery);
+
+    const connectionIds = connectionIdQuery.Items;
 
     const apig = new AWS.ApiGatewayManagementApi({
       apiVersion: '2018-11-29',
       endpoint: 'https://' + event.requestContext.domainName + '/' + event.requestContext.stage
     });
 
-    // const postCalls = 
+    const postCalls = connectionIds.map(async (connectionId) => {
+      console.log(connectionId);
+      const params = {
+        ConnectionId: connectionId.ConnectionId.S,
+        Data: JSON.stringify(payload)
+      };
+
+      try {
+        await apig.postToConnection(params).promise();
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    try {
+      await Promise.all(postCalls);
+    } catch (error) {
+      callback(null, { statusCode: 500, body: error.stack });
+    }
+
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'messages sent' })
+    });
   } catch (error) {
     callback(null, {
       statusCode: 500,
