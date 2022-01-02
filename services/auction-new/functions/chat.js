7bc4c737d4aa149cd7f145f5ccef13875808e323
Chris Burke <burke1791@gmail.com>
2022-01-01 18:48:09 -0600

    MMC-1: added getAllMessages function

---
+++ services/auction-new/functions/chat.js
@@ -0,0 +1,50 @@
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB();
+const lambda = new AWS.Lambda();
+
+const CHAT_TABLE = process.env.CHAT_TABLE;
+const LAMBDAS = {
+  VERIFY_USER_LEAGUE: `calcutta-auction-service-v2-${process.env.APP_ENV}-verifyUserLeague`
+};
+
+export async function getAllMessages(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const cognitoSub = event.cognitoPoolClaims.sub;
+  const leagueId = event.path.leagueId;
+
+  try {
+    const lambdaParams = {
+      FunctionName: LAMBDAS.VERIFY_USER_LEAGUE,
+      LogType: 'Tail',
+      Payload: JSON.stringify({ leagueId: leagueId, cognitoSub: cognitoSub })
+    };
+
+    const lambdaResponse = await lambda.invoke(lambdaParams).promise();
+    const responsePayload = JSON.parse(lambdaResponse.Payload);
+
+    if (!responsePayload.length) {
+      console.log('User not found - throw');
+      throw new Error('Could not find a matching registered user');
+    }
+
+    const messagesQuery = {
+      TableName: CHAT_TABLE,
+      Key: {
+        LeagueId: {
+          N: leagueId
+        }
+      }
+    };
+
+    const response = await dynamodb.query(messagesQuery).promise();
+
+    console.log(response);
+
+    callback(null, response.Items);
+  } catch (error) {
+    console.log(error);
+    callback(null, error);
+  }
+}
