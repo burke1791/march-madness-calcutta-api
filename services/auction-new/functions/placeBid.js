90dd400a80f066cb1ed9fc1a76073d5254586b2e
Chris Burke <burke1791@gmail.com>
2022-01-15 08:40:15 -0600

    MMC-5: removed console log

--- services/auction-new/functions/placeBid.js
+++ services/auction-new/functions/placeBid.js
@@ -7,8 +7,6 @@ const dynamodb = new AWS.DynamoDB();
 export async function placeBid(event, context, callback) {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  console.log(DYNAMODB_TABLES);
-
   const data = JSON.parse(event.body);
   const leagueId = data.leagueId;
   const connectionId = event.requestContext.connectionId;
