93b01204b7ff36357b33fa32850417cb1af4b58c
Chris Burke <burke1791@gmail.com>
2022-03-06 07:35:01 -0600

    MMC-37: return active and inactive leagues in separate arrays

--- services/league/functions/getLeagueSummaries.js
+++ services/league/functions/getLeagueSummaries.js
@@ -16,9 +16,26 @@ export async function getLeagueSummaries(event, context, callback) {
       .input('CognitoSub', Varchar(256), cognitoSub)
       .execute('dbo.up_GetLeagueSummaries');
 
-    callback(null, result.recordset);
+    const leagues = {
+      active: parseActiveLeagues(result.recordset),
+      inactive: parseInactiveLeagues(result.recordset)
+    };
+
+    callback(null, leagues);
   } catch (error) {
     console.log(error);
     callback(null, { message: 'ERROR!' });
   }
+}
+
+function parseActiveLeagues(leagues) {
+  return leagues.filter(league => {
+    return league.StatusId != 4;
+  });
+}
+
+function parseInactiveLeagues(leagues) {
+  return leagues.filter(league => {
+    return league.StatusId == 4;
+  })
 }
