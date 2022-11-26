b39fd8fb88db3f987abec8b85e9842bb270cd7ae
Chris Burke <burke1791@gmail.com>
2022-11-26 06:01:15 -0600

    MMC-91: get MM bracket endpoint

---
+++ services/tournament/functions/march-madness/getMarchMadnessBracket.js
@@ -0,0 +1,37 @@
+import { BigInt, Varchar } from '../../../../common/utilities/db';
+import { parseTournamentTree } from '../../utilities/helper';
+
+const connection = require('../../../../common/utilities/db').connection;
+
+export async function getMarchMadnessBracket(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const cognitoSub = event.cognitoPoolClaims.sub;
+  const leagueId = event.path.leagueId;
+
+  try {
+    if (!connection.isConnected) {
+      await connection.createConnection();
+    }
+
+    const result = await connection.pool.request()
+      .input('LeagueId', BigInt, leagueId)
+      .input('CognitoSub', Varchar(256), cognitoSub)
+      .execute('dbo.up_TournamentPage_MarchMadnessBracket');
+
+    const data = {};
+
+    if (result.recordset[0]?.Error == 'No bracket available') {
+      data.errorMessage = 'No bracket available';
+    } else {
+      const bracket = parseTournamentTree(result.recordsets[1]);
+      data.bracketMetadata = result.recordsets[0][0];
+      data.bracket = bracket;
+    }
+
+    callback(null, data);
+  } catch (error) {
+    console.log(error);
+    callback(null,  { message: 'SERVER ERROR!' });
+  }
+}
