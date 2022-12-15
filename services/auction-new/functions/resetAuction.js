9e347885b33631b80fed1f162a1c409e84f747ed
Chris Burke <burke1791@gmail.com>
2022-12-15 11:09:28 -0600

    MMC-86: commented out unused vars

--- services/auction-new/functions/resetAuction.js
+++ services/auction-new/functions/resetAuction.js
@@ -1,21 +1,21 @@
 import AWS from 'aws-sdk';
 import { DYNAMODB_TABLES, LAMBDAS } from '../utilities/constants';
 
-const lambda = new AWS.Lambda();
+// const lambda = new AWS.Lambda();
 const dynamodb = new AWS.DynamoDB();
 
 export async function resetAuction(event, context, callback) {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  const cognitoSub = event.cognitoPoolClaims.sub;
+  // const cognitoSub = event.cognitoPoolClaims.sub;
   const leagueId = event.path.leagueId;
 
   try {
-    const lambdaParams = {
-      FunctionName: LAMBDAS.RDS_RESET_AUCTION,
-      LogType: 'Tail',
-      Payload: JSON.stringify({ leagueId: leagueId, cognitoSub: cognitoSub })
-    }
+    // const lambdaParams = {
+    //   FunctionName: LAMBDAS.RDS_RESET_AUCTION,
+    //   LogType: 'Tail',
+    //   Payload: JSON.stringify({ leagueId: leagueId, cognitoSub: cognitoSub })
+    // }
 
     // const lambdaResponse = await lambda.invoke(lambdaParams).promise();
     // const responsePayload = JSON.parse(lambdaResponse.Payload);
@@ -25,14 +25,14 @@ export async function resetAuction(event, context, callback) {
     // }
 
     // delete the auction record in dynamodb
-    const deleteAuctionParams = {
-      TableName: DYNAMODB_TABLES.AUCTION_TABLE,
-      Key: {
-        LeagueId: {
-          N: String(leagueId)
-        }
-      }
-    }
+    // const deleteAuctionParams = {
+    //   TableName: DYNAMODB_TABLES.AUCTION_TABLE,
+    //   Key: {
+    //     LeagueId: {
+    //       N: String(leagueId)
+    //     }
+    //   }
+    // }
 
     // const deleteAuctionResponse = await dynamodb.deleteItem(deleteAuctionParams).promise();
     // console.log(deleteAuctionResponse);
