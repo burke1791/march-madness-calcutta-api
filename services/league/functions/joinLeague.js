f96890009b46bbd648808cebdac809dcfe4db791
Chris Burke <burke1791@gmail.com>
2024-01-29 12:19:48 -0600

    MMC-161: first attempt

--- services/league/functions/joinLeague.js
+++ services/league/functions/joinLeague.js
@@ -1,4 +1,8 @@
+import AWS from 'aws-sdk';
 import { Varchar } from '../../../common/utilities/db';
+import { LAMBDAS } from '../utilities/constants';
+
+const lambda = new AWS.Lambda();
 
 const connection = require('../../../common/utilities/db').connection;
 
@@ -19,14 +23,40 @@ export async function joinLeague(event, context, callback) {
       .input('LeagueGuid', Varchar(256), inviteCode)
       .execute('dbo.up_JoinLeague');
 
+    const response = [];
+
     if (result.recordset[0]?.Error == undefined) {
-      let leagueId = result.recordset[0].LeagueId;
-      result.recordset[0].LeaguePath = `/leagues/${leagueId}`;
+      const leagueId = result.recordset[0].LeagueId;
+
+      await syncLeagueMembershipData(leagueId, result.recordset);
+
+      response.push({ 
+        LeagueId: leagueId,
+        LeaguePath: `/leagues/${leagueId}`
+      });
+    } else {
+      response.push({ Error: result.recordset[0].Error });
     }
 
-    callback(null, result.recordset);
+    callback(null, response);
   } catch (error) {
     console.log(error);
     callback(null, error);
+  }
+}
+
+async function syncLeagueMembershipData(leagueId, leagueMemberships) {
+  const lambdaParams = {
+    FunctionName: LAMBDAS.SYNC_LEAGUE_MEMBERSHIP,
+    LogType: 'Tail',
+    Payload: JSON.stringify({ leagueId: leagueId, leagueMemberships: leagueMemberships })
+  };
+
+  try {
+    const lambdaResponse = await lambda.invoke(lambdaParams).promise();
+    console.log(lambdaResponse);
+  } catch (error) {
+    console.log(error);
+    console.log('Unable to sync league membership. Should I undo the SQL Server action?');
   }
 }
---
+++ services/league/utilities/constants.js
@@ -0,0 +1,3 @@
+export const LAMBDAS = {
+  SYNC_LEAGUE_MEMBERSHIP: `calcutta-auction-service-v2-${process.env.APP_ENV}-syncLeagueMembership`
+};
