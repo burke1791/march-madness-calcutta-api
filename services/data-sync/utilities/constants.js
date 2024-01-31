cd7c8ebb3dd62e5504597df0061eaa9dd4e15cbf
Chris Burke <burke1791@gmail.com>
2024-01-31 10:36:19 -0600

    MMC-161: added appropriate roles

---
+++ services/data-sync/utilities/constants.js
@@ -0,0 +1,4 @@
+export const LAMBDAS = {
+  SYNC_LEAGUE_MEMBERSHIP: `calcutta-auction-service-v2-${process.env.APP_ENV}-syncLeagueMembership`,
+  SQL_JOIN_LEAGUE: `calcutta-league-service-${process.env.APP_ENV}-joinLeague`
+};
--- services/league/utilities/constants.js
+++
@@ -1,3 +0,0 @@
-export const LAMBDAS = {
-  SYNC_LEAGUE_MEMBERSHIP: `calcutta-auction-service-v2-${process.env.APP_ENV}-syncLeagueMembership`
-};
