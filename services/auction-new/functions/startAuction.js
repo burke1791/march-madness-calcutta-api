4f877927b70b796886b262862a611ad15170df18
Chris Burke <burke1791@gmail.com>
2024-03-02 02:54:36 -0600

    MMC-158: fixed bug

--- services/auction-new/functions/startAuction.js
+++ services/auction-new/functions/startAuction.js
@@ -32,7 +32,7 @@ export async function startAuction(event, context, callback) {
     const responsePayload = JSON.parse(lambdaResponse.Payload);
     console.log(responsePayload);
     
-    if (!responsePayload.length || !!responsePayload[0]?.Error) {
+    if (!responsePayload?.length || !!responsePayload[0]?.Error) {
       throw new Error('Could not set league status');
     }
 
